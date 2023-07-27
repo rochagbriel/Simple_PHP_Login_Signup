960fc56f3a2303b92bddfdcc6e40429b1984ce29
Gabriel Rocha <rochagbriel@gmail.com>
2023-07-28 00:55:40 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>BAEBECA_TEST</title>
+</head>
+<body>
+    <form action="">
+        <input type="email" name="email" placeholder="Email">
+        <input type="password" name="password" placeholder="Password">
+        <input type="submit" name="submit" value="Submit">
+    </form>
+    <a href="register.php">Register</a>
+</body>
+</html>
+```
---
+++ register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>BAEBECA_TEST - Register</title>
+</head>
+<body>
+    <form action="">
+        <input type="email" name="email" placeholder="Email">
+        <input type="password" name="password" placeholder="Password">
+        <input type="password" name="password2" placeholder="Confirm Password">
+        <input type="submit" name="submit" value="Submit">
+    </form>
+    <a href="index.php">Login</a>
+</body>
+</html>
+```
