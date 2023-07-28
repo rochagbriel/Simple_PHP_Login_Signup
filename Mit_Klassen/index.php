2a339a8611bbebc68cb5ac2c91e9c774eaafcd29
Gabriel Rocha <rochagbriel@gmail.com>
2023-07-28 23:08:44 +0200

    Prepared to be Refactored

--- Mit_Klassen/index.php
+++ Mit_Klassen/index.php
@@ -1,15 +1,14 @@
-<!DOCTYPE html>
 <html>
 <head>
-    <title>Test_Baebeca_MainPage</title>
+    <title>Test_Baebeca_MainPage(mit Klassen)</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
     <section class="container-main">
-        <h1>Welcome to the Baebeca Test</h1>
+        <h1>Welcome to the Baebeca Test(mit Klassen)</h1>
         <ul>
-            <li><a class="btn" href="register.html">Register</a></li>
-            <li><a class="btn" href="login.html">Login</a></li>
+            <li><a class="btn" href="register.php">Register</a></li>
+            <li><a class="btn" href="login.php">Login</a></li>
         </ul>
     </section>
 </body>
