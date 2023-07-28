0ffb38d74bf4ac85454995d4f959192ca03ec9ec
Gabriel Rocha <rochagbriel@gmail.com>
2023-07-28 02:01:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta name="description" content="This is an example of a meta description. This will often show up in search results.">
     <meta name=viewport content="width=device-width, initial-scale=1">
     <title>BAEBECA_TEST</title>
 </head>
+
 <body>
     <form action="">
         <input type="email" name="email" placeholder="Email">
@@ -18,4 +20,5 @@
     echo '<p>test</p>';
     ?>
 </body>
+
 </html>
